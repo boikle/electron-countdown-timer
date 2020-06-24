b1480dad23acb7c139d98a6b07ca9496b5e5f26e
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-06-23 20:24:33 -0400

    Added created helper function for creating about page window.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
 const {app, BrowserWindow, Menu} = require('electron');
 
-function createWindow () {
+function createAboutWindow() {
+	let aboutWindow = new BrowserWindow({
+		width: 300,
+		height: 300,
+		frame: false,
+		alwaysOnTop: true,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	aboutWindow.loadFile('src/about.html');
+}
+
+function createMainWindow () {
 	// Create a browser window.
 	let mainWindow = new BrowserWindow({
 		width: 600,
@@ -13,6 +26,7 @@ function createWindow () {
 	// Load index.html file into electron browser window
 	mainWindow.loadFile('src/index.html');
 
+	// Define the main window's menu bar
 	let menu = Menu.buildFromTemplate([
 		{
 			label: 'Menu',
@@ -31,16 +45,7 @@ function createWindow () {
 				{
 					label: "About",
 					click() {
-						let aboutWindow = new BrowserWindow({
-							width: 300,
-							height: 300,
-							webPreferences: {
-								nodeIntegration: true
-							}
-
-						});
-						aboutWindow.loadFile('src/about.html');
-						aboutWindow.removeMenu();
+						createAboutWindow();
 					}
 				}
 			]
@@ -51,7 +56,7 @@ function createWindow () {
 }
 
 // When ready, create the app window
-app.whenReady().then(createWindow);
+app.whenReady().then(createMainWindow);
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
@@ -65,6 +70,6 @@ app.on('activate', () => {
 	// In macOs, recreate a window if the dock icon is clicked and there are
 	// no other windows open.
 	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
+		createMainWindow();
 	}
 });
