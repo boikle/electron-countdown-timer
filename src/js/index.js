d999be56701e309cdeda955087ee5fc1e8497d6b
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-06-28 19:10:06 -0400

    Updated updateTimer function and added a helper function to convert from remaining seconds to a string used by the timer.

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,6 @@ const CountdownTimer = require('./js/countdown_timer');
 const CountdownControlPanel = require('./js/countdown_control_panel');
 
 const timer = new CountdownTimer();
-timer.updateCountdownDisplay();
-
 const control_panel = new CountdownControlPanel();
 
 // Add play button to control panel
