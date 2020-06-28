269d984ceac486092ee2d6480fe7016ed8d26104
boikle <boikle@users.noreply.github.com>
2020-06-28 19:10:06 -0400

    Updated updateTimer function and added a helper function to convert from remaining seconds to a string used by the timer.

--- src/js/countdown_timer.js
+++ src/js/countdown_timer.js
@@ -6,6 +6,27 @@ class CountdownTimer {
 		document.addEventListener('startTimer', function() {
 			console.log('Play button was pushed!');
 		});
+
+		// Update timer to display
+		this.updateTimer();
+	}
+
+	convertSStoHHMMSS(totalsecs) {
+		let hours = Math.floor(totalsecs / 3600);
+		let minutes = Math.floor((totalsecs - (hours * 3600)) / 60);
+		let seconds = Math.floor(totalsecs - (hours * 3600) - (minutes * 60));
+
+		// Format hours, minutes, and seconds values for timer
+		hours = hours < 10 ? "0" + hours : hours;
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		return hours + ":" + minutes + ":" + seconds;
+	}
+
+	updateTimer() {
+		let timerText = this.convertSStoHHMMSS(this.remainingTime);
+		this.timer.textContent = timerText;
 	}
 
 	getCountdownTime() {
@@ -16,10 +37,6 @@ class CountdownTimer {
 		if (typeof(secs) === 'number') {
 			this.remainingTime = secs;
 		}
-	}
-
-	updateCountdownDisplay() {
-		this.timer.textContent = "00:05:00";
 	}
 }
 
