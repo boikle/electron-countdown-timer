979b71f88c152d5877a561c1028a890f49af6915
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-06-28 19:58:53 -0400

    Added startTimer function to initialize the countdown sequence.
    Also added documentation for functions.

--- src/js/countdown_timer.js
+++ src/js/countdown_timer.js
@@ -1,41 +1,82 @@
+/** Class representing a coundown timer */
 class CountdownTimer {
+	/**
+	* Constructor for a countdown timer object.
+	* @constructor
+	*/
 	constructor() {
+		this.countdownInterval = null;
+		let _this = this;
 		this.timer = document.getElementById('countdown_timer');
-		this.remainingTime = 300; // 5 minutes
+		this.remainingTime = 300000; // 5 minutes in ms.
 
 		document.addEventListener('startTimer', function() {
-			console.log('Play button was pushed!');
+			_this.startTimer();
+		});
+
+		document.addEventListener('pauseTimer', function() {
+			window.clearInterval(this.countdownInterval);
 		});
 
 		// Update timer to display
 		this.updateTimer();
 	}
 
-	convertSStoHHMMSS(totalsecs) {
-		let hours = Math.floor(totalsecs / 3600);
-		let minutes = Math.floor((totalsecs - (hours * 3600)) / 60);
-		let seconds = Math.floor(totalsecs - (hours * 3600) - (minutes * 60));
-
-		// Format hours, minutes, and seconds values for timer
-		hours = hours < 10 ? "0" + hours : hours;
-		minutes = minutes < 10 ? "0" + minutes : minutes;
-		seconds = seconds < 10 ? "0" + seconds : seconds;
-
-		return hours + ":" + minutes + ":" + seconds;
+	/**
+	* Start the countdown timer, updating the timer text every second.
+	*/
+	startTimer() {
+		let _this = this;
+		let interval = 1000; // milliseconds
+		let endTime = Date.now() + (this.remainingTime);
+		this.countdownInterval = window.setInterval(function() {
+			let remainingTime = endTime - Date.now();
+			_this.setCountdownTime(remainingTime);
+			_this.updateTimer();
+		}, interval);
 	}
 
+	/**
+	* Converts remaing time into a string used by the countdown timer
+	* The countdown timer uses the format of HH:MM:SS.
+	* @param {number} remainingTime - The remaining time in ms.
+	*/
+	convertMStoHHMMSS(ms) {
+		let hh = Math.floor(ms / 3600000);
+		let mm = Math.floor((ms - (hh * 3600000)) / 60000);
+		let ss = Math.floor(ms - (hh * 3600000) - (mm * 60000)) / 1000;
+
+		// Format hours, minutes, and seconds values for timer
+		hh = hh < 10 ? "0" + hh : hh;
+		mm = mm < 10 ? "0" + mm : mm;
+		ss = ss < 10 ? "0" + ss : ss;
+
+		return hh + ":" + mm + ":" + ss;
+	}
+
+	/**
+	* Update the countdown timer textContent based on the remaing time value.
+	*/
 	updateTimer() {
-		let timerText = this.convertSStoHHMMSS(this.remainingTime);
+		let timerText = this.convertMStoHHMMSS(this.remainingTime);
 		this.timer.textContent = timerText;
 	}
 
+	/**
+	* Get the remaining time on the countdown timer.
+	* @return {number} remaining time in ms.
+	*/
 	getCountdownTime() {
 		return this.remainingTime;
 	}
 
-	setCountdownTime(secs) {
-		if (typeof(secs) === 'number') {
-			this.remainingTime = secs;
+	/**
+	* Set the remaining time on the countdown timer.
+	* @param {number} ms - Milliseconds
+	*/
+	setCountdownTime(ms) {
+		if (typeof(ms) === 'number') {
+			this.remainingTime = ms;
 		}
 	}
 }
