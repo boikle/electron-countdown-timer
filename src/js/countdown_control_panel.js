ad09400a6b612e6a543adf419b85fa949512a5ed
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-06-29 20:19:08 -0400

    Broke out play/pause button's toggle logic
    Also added documentation for class and functions.

--- src/js/countdown_control_panel.js
+++ src/js/countdown_control_panel.js
@@ -1,30 +1,47 @@
+/** Class representing the countdown timer's control panel */
 class CountdownControlPanel {
+	/**
+	* Constructor for the Countdown Timer's Control Panel
+	* @constructor
+	*/
 	constructor() {
 		this.state = "off";
+		this.playBtn;
 	}
 
+	/**
+	* Adds a play button to the control panel
+	*/
 	addPlayButton() {
 		let _this = this;
 		const panel = document.getElementById('countdown_control_panel');
-		let playBtn = document.createElement('div');
-		playBtn.innerHTML = '\u25B6';
-		playBtn.setAttribute('id', 'playbtn');
-		playBtn.addEventListener('click', function() {
-			if (_this.state === "off") {
-				var startEvent = new CustomEvent("startTimer");
-				document.dispatchEvent(startEvent);
-				_this.state = "on";
-				this.innerHTML = '\u23F8';
-
-			} else {
-				var pauseEvent = new CustomEvent("pauseTimer");
-				document.dispatchEvent(pauseEvent);
-				_this.state = "off";
-				this.innerHTML = '\u25B6';
-			}
+		this.playBtn = document.createElement('div');
+		this.playBtn.innerHTML = '\u25B6';
+		this.playBtn.setAttribute('id', 'playbtn');
+		this.playBtn.addEventListener('click', function() {
+			_this.togglePlayButtonState();
 		});
 
-		panel.appendChild(playBtn);
+		panel.appendChild(this.playBtn);
+	}
+
+	/**
+	* Helper function to perform toggle logic when play/pause button is clicked
+	*/
+	togglePlayButtonState() {
+		let event;
+		if (this.state === "off") {
+			event = new CustomEvent("startTimer");
+			document.dispatchEvent(event);
+			this.state = "on";
+			this.playBtn.innerHTML = '\u23F8';
+
+		} else {
+			event = new CustomEvent("pauseTimer");
+			document.dispatchEvent(event);
+			this.state = "off";
+			this.playBtn.innerHTML = '\u25B6';
+		}
 	}
 
 	addConfigButton() {
