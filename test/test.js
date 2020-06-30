17bb41f9dc9a92d2fc3b65db07a1bf26dfe12861
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-06-29 20:35:33 -0400

    Added/updated countdown timer tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,30 @@
 const Timer = require('../src/js/countdown_timer');
 const assert = require('assert');
 
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+const dom = new JSDOM(
+	`<!DOCTYPE HTML>
+    <html lang="en">
+	   <head>
+           <meta charset="UTF-8">
+           <link rel="stylesheet" href="css/window.css">
+		   <link rel="stylesheet" href="css/timer.css">
+		   <link rel="stylesheet" href="css/control_panel.css">
+		   <title>Countdown Timer</title>
+	    </head>
+	    <body>
+		    <div id="countdown">
+			    <div id="countdown_timer">00:00:00</div>
+			    <div id="countdown_control_panel"></div>
+		    </div>
+		    <script src="js/index.js"></script>
+	    </body>
+    </html>`
+);
+
+global.document = dom.window.document;
+
 // Countdown timer tests
 describe('Timer', function() {
     let timer = new Timer();
@@ -9,8 +33,15 @@ describe('Timer', function() {
             assert.equal(typeof timer.getCountdownTime(), "number");
         });
 
-        it('The default initial value should be 300', function(){
-            assert.equal(timer.getCountdownTime(), 300);
+        it('The default initial value should be 300000', function() {
+            assert.equal(timer.getCountdownTime(), 300000);
+        });
+    });
+
+    describe('setCountdownTime()', function() {
+        it('The timer value should be updated to the set value', function() {
+            timer.setCountdownTime(500000);
+            assert.equal(timer.getCountdownTime(), 500000);
         });
     });
 });
