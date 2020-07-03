d3ceada186828a0b9dc025268f6bb1aa18e57025
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-07-03 16:35:56 -0400

    Added Inter Process Communication between main and renderer windows for updating the countdown timer value.

--- src/js/config_window.js
+++ src/js/config_window.js
@@ -1,10 +1,32 @@
-const electron = require('electron');
-const remote = electron.remote;
+const {ipcRenderer, remote} = require('electron');
 const set_timer_btn = document.getElementById('set_timer_btn');
+
+/**
+* Convert Hours (HH), Minutes (MM), and Seconds (SS) values
+* provided by config window input fields, and converts the combination
+* to milliseconds.
+* @return
+*/
+function convertHHMMSSInputsToMS() {
+	let MS;
+	let HH = document.getElementById('hh').value;
+	let MM = document.getElementById('mm').value;
+	let SS = document.getElementById('ss').value;
+
+	MS = (HH * 3600000) + (MM * 60000) + (SS * 1000);
+	return MS;
+}
 
 // Add Event Handler for set timer button
 set_timer_btn.addEventListener('click', function() {
-	// close window
-	let window = remote.getCurrentWindow();
-	window.close();
+	let config_window = remote.getCurrentWindow();
+
+	// Convert input values to Milliseconds
+	let MS = convertHHMMSSInputsToMS();
+
+	// Send milliseconds value using inter process communication
+	ipcRenderer.send('configTimer', MS.toString());
+
+	// Close window
+	config_window.close();
 });
--- src/js/index_window.js
+++ src/js/index_window.js
@@ -1,9 +1,16 @@
+const {ipcRenderer} = require('electron');
 const CountdownTimer = require('./js/countdown_timer');
 const CountdownControlPanel = require('./js/countdown_control_panel');
 
-new CountdownTimer();
-
+const timer = new CountdownTimer();
 const control_panel = new CountdownControlPanel();
 
 // Add play button to control panel
 control_panel.addPlayButton();
+
+// Listen for configTimer events, and update the timer with updated time.
+ipcRenderer.on('configTimer', function(event, arg) {
+	timer.pauseTimer();
+	timer.setCountdownTime(Number(arg));
+	timer.updateTimer();
+});
