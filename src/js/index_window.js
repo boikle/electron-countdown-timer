614aa7e57a133b6964221c6734e15a068e7a0fba
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-07-06 20:48:54 -0400

    Instatiate control panel inside countdown timer class.
    - Toggle state of button when timer is updated, or finished.
    - Added logic to stop timer when 0ms is reached.

--- src/js/index_window.js
+++ src/js/index_window.js
@@ -1,16 +1,12 @@
 const {ipcRenderer} = require('electron');
 const CountdownTimer = require('./js/countdown_timer');
-const CountdownControlPanel = require('./js/countdown_control_panel');
 
 const timer = new CountdownTimer();
-const control_panel = new CountdownControlPanel();
-
-// Add play button to control panel
-control_panel.addPlayButton();
 
 // Listen for configTimer events, and update the timer with updated time.
 ipcRenderer.on('configTimer', function(event, arg) {
 	timer.pauseTimer();
+	timer.controlPanel.togglePlayButtonState();
 	timer.setCountdownTime(Number(arg));
 	timer.updateTimer();
 });
